d8b8abef82961bb6b7329d2f0c0d66c4389d1713
Neil Jassal <neil.jassal@gmail.com>
2017-03-03 22:59:06 -0500

    Conda environment for preprocessing

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -7,3 +7,16 @@ Updated 3/3/2017
 Preprocessing of DSB 2017 data. Based on:
 https://www.kaggle.com/gzuidhof/data-science-bowl-2017/full-preprocessing-tutorial
 """
+import os
+
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, csv I/O
+import dicom
+import scipy.ndimage
+import matplotlib.pyplot as plt
+
+from skimage import measure, morphology
+# from mpl_toolkits.mplot3d.art3d import Poly3DCollection  # TODO need this?
+
+INPUT_FOLDER = "data\\sample_images\\"
+patients = os.listdir(INPUT_FOLDER).sort()
